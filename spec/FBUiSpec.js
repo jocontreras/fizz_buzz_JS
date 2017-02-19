a62b1f1491fae21747708426a74650192c1e592a
jocontreras <jen.almarasy@gmail.com>
2017-02-19 22:47:31 +0100

    added user interface spec

---
+++ spec/FBUiSpec.js
@@ -0,0 +1,8 @@
+describe('FizzBuss_UI - index.html', function() {
+    beforeEach(function() {
+        jasmine.getFixtures().fixturesPath = '.';
+        loadFixtures('index.html');
+        $.holdReady(false);
+    });
+
+});
